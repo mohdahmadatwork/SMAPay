42a68bf10effff1dfed6edb27b3cea2c1d7f74ae
Mohd Ahmad <mohdahmadatwork@gmail.com>
2024-04-19 17:43:00 +0530

    Added CI/CD pipeline

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Providers } from "../provider";
+import { AppbarClient } from "../components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={inter.className}>
+          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
+            <AppbarClient />
+            {children}
+          </div>
+        </body>
+      </Providers>
     </html>
   );
 }
