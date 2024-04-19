8d25c24b279e773e13d8689f2da4daa6493ddc55
Mohd Ahmad <mohdahmadatwork@gmail.com>
2024-04-19 18:25:43 +0530

    minor bug fix

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -78,9 +78,7 @@ export default function Page(): JSX.Element {
         </div>
       </div>
 
-      <Button onClick={()=>{}}>
-        Click me!
-      </Button>
+
 
       <div className={styles.hero}>
         <div className={styles.heroContent}>
